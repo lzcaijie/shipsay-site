0a6c262a3651d2be50a8f3684ae18b55917c09a4
root <root@api.bt.cn>
2026-03-09 17:00:32 +0800

    chore: biquge v2 mobile top direct display

--- www/static/biquge/common.js
+++ www/static/biquge/common.js
@@ -1,15 +1,3 @@
 function gotop(){$('body,html').animate({scrollTop:0},600);}
 function gofooter(){$('body,html').animate({scrollTop:$(document).height()},600);}
-function menu_toggle() {
-    $('nav, .search').slideToggle();
-    $('#menu-btn').text() == '菜单' ? $('#menu-btn').text('关闭') : $('#menu-btn').text('菜单')
-}
 function setEcho(){$("img.lazy").lazyload({effect : "fadeIn"})}
-function search(){
-    var action = (window.ssSearchAction || '').replace(/'/g, '&#39;');
-    if (action) {
-        document.write('<form class="flex" name="t_frmsearch" method="post" action="'+ action +'"><input id="searchkey" type="text" name="searchkey" class="search_input" placeholder="书名或作者,请您少字也别错字" autocomplete="off"><button type="submit" name="Submit" class="search_btn" title="搜索"> 搜 索 </button></form>');
-    } else {
-        document.write('<form class="flex" name="t_frmsearch" method="post" onsubmit="return false;"><input id="searchkey" type="text" name="searchkey" class="search_input" placeholder="书名或作者,请您少字也别错字" autocomplete="off"><button type="submit" name="Submit" class="search_btn" title="搜索" disabled="disabled" aria-disabled="true"> 搜 索 </button></form>');
-    }
-}
