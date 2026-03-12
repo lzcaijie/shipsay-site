eb4e3c01a5628d69f2577d281b2b2b0bc4373544
root <root@api.bt.cn>
2026-03-12 18:47:28 +0800

    fix: 收口 youdu 手机端详情目录样式

--- themes/youdu/tpl_footer.php
+++ themes/youdu/tpl_footer.php
@@ -5,9 +5,9 @@ $footer_site_name = htmlspecialchars((string)SITE_NAME, ENT_QUOTES, 'UTF-8');
 ?>
 <p class="g_hide">本站所有小说作品均来自网友上传或转载于其他网站，版权属于原创作者</p>
 <p class="g_hide">如侵犯了您的权利，请与本站联系，本站将立刻删除</p>
-<p><?=$footer_site_name?>所有小说均由根据搜索引擎转码而来，只为让更多读者欣赏。</p>
-<p>本站不保存小说内容及数据，仅作宣传展示，请在阅读后支持正版作品。</p>
-<p><?=$footer_site_name?>基于搜索引擎技术为您提供检索服务，版权投诉删除及小说建议请联系我们。</p>
+<p><?=$footer_site_name?>小说均来自搜索引擎转码，仅供检索与展示。</p>
+<p>本站不保存小说正文，请阅读后支持正版作品。</p>
+<p>版权投诉、删除申请、内容建议，请联系站长处理。</p>
 <p>Copyright &copy; <?=$footer_year?> <?=$footer_site_name?></p>
 <p>
     网站地图：
